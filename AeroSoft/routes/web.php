bcad86dd8d815970b9fd9779646caacd1c3a63e9
Luis Fernando Acosta Vega <leoescorp2552@gmail.com>
2025-10-22 14:55:43 -0500

    Commit, subir funciones de login

---
+++ AeroSoft/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\AdminControl;
+use App\Http\Controllers\LoginControl;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('Practica.Index');
+});
+
+Route::get('/Login', [LoginControl::class, 'MostrarLogin'])
+    ->name('login')
+    ->middleware('Prevenir');
+
+Route::post('/Login/Login', [LoginControl::class, 'Login'])->name('loguear');
+
+Route::post('/Logout', [LoginControl::class, 'Logout'])
+    ->name('logout')
+    ->middleware('Prevenir');
+
+Route::middleware(['auth','Prevenir'])->group(function() {
+    Route::middleware('rol:1')->get('/Admin/Index', [AdminControl::class, 'Index'])->name('admin_index');
+    Route::middleware('rol:1')->get('/Admin/Lista', [AdminControl::class, 'ListaEditor'])->name('admin_lista');
+    Route::middleware('rol:1')->get('/Admin/Registrar', [AdminControl::class, 'Registrar'])->name('admin_registrar');
+    Route::middleware('rol:1')->post('/Admin/Registro', [AdminControl::class, 'Registro'])->name('admin_registro');
+});
